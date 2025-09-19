89f76fd9ef442a60e91c3fadaba1fa055d5c49e1
Kezreux <aanestadnicholas@gmail.com>
2025-09-19 23:43:12 +0200

    Testing the tank volume functions

--- functions/tank.py
+++ functions/tank.py
@@ -1,17 +1,35 @@
 
+import math
+
+# This code calculates the total amount of areal inside the tank.
+# which can further calculate into volume based on the viscosity
+
+class TankParameters:
+   
+    # Default starting values for the simulator
+    tank_cylinder_height: float = 5.0
+    tank_radius: float = 1.5
+
+    # Function to calculate the inner areal of the cylindrical part of the tank
+    def tank_cylinder_volume(self) -> float:
+        return (self.tank_radius ** 2) * math.pi * self.tank_cylinder_height
+
+    # Function to calculate the inner areal of the hemisphere at the bottom of the tank
+    def tank_hemisphere_volume(self) -> float:
+        return ((2.0 / 3.0) * math.pi * (self.tank_radius ** 3))
+
+    # Function to calculate the total volume capabilities of the tank
+    def tank_total_volume(self) -> float:
+        return self.tank_cylinder_volume() + self.tank_hemisphere_volume()
+
+    if __name__ == "__main__":
+        tank = TankParameters()
+        print(f"Cylinder volume:    {tank.tank_cylinder_volume()}")
+        print(f"Hemisphere volume : {tank.tank_hemisphere_volume()}")
+        print(f"Total volume:       {tank.tank_total_volume()}")
 
 
-class TankParamaters:
-
-    areal: float
-    height: float
-    radius: float
-
-
-    def TankSylinderAreal:
 
 
 
-
-    def HemisphereAreal
 
